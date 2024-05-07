4c649923d815def83f04f3e6e4b6a9e0a263e281
zhouzhangqi <1978892854@qq.com>
2024-05-07 22:05:12 +0800

    features: support encode

---
+++ src/resp/mod.rs
@@ -0,0 +1,141 @@
+mod decode;
+mod encode;
+
+use enum_dispatch::enum_dispatch;
+use std::collections::HashMap;
+use std::ops::{Deref, DerefMut};
+
+#[enum_dispatch]
+pub trait RespEncode {
+    fn encode(self) -> Vec<u8>;
+}
+
+pub trait RespDecode {
+    fn decode(self) -> Result<RespFrame, String>;
+}
+
+#[enum_dispatch(RespEncode)]
+pub enum RespFrame {
+    SimpleString(SimpleString),
+    Error(SimpleError),
+    Integer(i64),
+    BulkString(BulkString),
+    NullBulkString(RespNullBulkString),
+    Array(RespArray),
+    Null(RespNull),
+    NullArray(RespNullArray),
+    Boolean(bool),
+    Double(f64),
+    Map(RespMap),
+    Set(RespSet),
+}
+
+pub struct SimpleString(String);
+pub struct SimpleError(String);
+
+pub struct BulkString(Vec<u8>);
+
+pub struct RespArray(Vec<RespFrame>);
+
+pub struct RespNull;
+pub struct RespNullArray;
+
+pub struct RespNullBulkString;
+
+pub struct RespMap(HashMap<String, RespFrame>);
+
+pub struct RespSet(Vec<RespFrame>);
+
+impl Deref for SimpleString {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for SimpleError {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for BulkString {
+    type Target = Vec<u8>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespArray {
+    type Target = Vec<RespFrame>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for RespMap {
+    type Target = HashMap<String, RespFrame>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for RespMap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Deref for RespSet {
+    type Target = Vec<RespFrame>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl SimpleString {
+    pub fn new(s: impl Into<String>) -> Self {
+        SimpleString(s.into())
+    }
+}
+
+impl SimpleError {
+    pub fn new(s: impl Into<String>) -> Self {
+        SimpleError(s.into())
+    }
+}
+
+impl BulkString {
+    pub fn new(s: impl Into<Vec<u8>>) -> Self {
+        BulkString(s.into())
+    }
+}
+
+impl RespArray {
+    pub fn new(s: impl Into<Vec<RespFrame>>) -> Self {
+        RespArray(s.into())
+    }
+}
+
+impl Default for RespMap {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl RespMap {
+    pub fn new() -> Self {
+        RespMap(HashMap::new())
+    }
+}
+
+impl RespSet {
+    pub fn new(s: impl Into<Vec<RespFrame>>) -> Self {
+        RespSet(s.into())
+    }
+}
