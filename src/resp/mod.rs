b103e80f93372a69b0482c42b2de2b47087d35bd
zhouzhangqi <1978892854@qq.com>
2024-05-09 22:00:54 +0800

    feature: support RespFrame decode

--- src/resp/mod.rs
+++ src/resp/mod.rs
@@ -1,20 +1,47 @@
 mod decode;
 mod encode;
 
+use bytes::{Buf, BytesMut};
 use enum_dispatch::enum_dispatch;
 use std::collections::HashMap;
 use std::ops::{Deref, DerefMut};
+use thiserror::Error;
+
+const CRLF: &[u8] = b"\r\n";
+const CRLF_LEN: usize = CRLF.len();
 
 #[enum_dispatch]
 pub trait RespEncode {
     fn encode(self) -> Vec<u8>;
 }
 
-pub trait RespDecode {
-    fn decode(self) -> Result<RespFrame, String>;
+pub trait RespDecode: Sized {
+    const PREFIX: &'static str;
+    fn decode(buf: &mut BytesMut) -> Result<Self, RespError>;
+    fn expect_length(buf: &[u8]) -> Result<usize, RespError>;
+}
+
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum RespError {
+    #[error("Invalid frame: {0}")]
+    InvalidFrame(String),
+    #[error("Invalid frame type: {0}")]
+    InvalidFrameType(String),
+    #[error("Invalid frame Length: {0}")]
+    InvalidFrameLength(isize),
+    #[error("Frame not complete")]
+    NotComplete,
+
+    #[error("Parse error: {0}")]
+    ParseIntError(#[from] std::num::ParseIntError),
+    #[error("Utf8 error: {0}")]
+    Utf8Error(#[from] std::string::FromUtf8Error),
+    #[error("Parse float error: {0}")]
+    ParseFloatError(#[from] std::num::ParseFloatError),
 }
 
 #[enum_dispatch(RespEncode)]
+#[derive(Debug, PartialEq)]
 pub enum RespFrame {
     SimpleString(SimpleString),
     Error(SimpleError),
@@ -29,21 +56,30 @@ pub enum RespFrame {
     Map(RespMap),
     Set(RespSet),
 }
-
+#[derive(Debug, PartialEq, Eq)]
 pub struct SimpleString(String);
+#[derive(Debug, PartialEq, Eq)]
 pub struct SimpleError(String);
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct BulkString(Vec<u8>);
 
+#[derive(Debug, PartialEq)]
 pub struct RespArray(Vec<RespFrame>);
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct RespNull;
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct RespNullArray;
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct RespNullBulkString;
 
+#[derive(Debug, PartialEq)]
 pub struct RespMap(HashMap<String, RespFrame>);
 
+#[derive(Debug, PartialEq)]
 pub struct RespSet(Vec<RespFrame>);
 
 impl Deref for SimpleString {
@@ -137,5 +173,100 @@ impl RespMap {
 impl RespSet {
     pub fn new(s: impl Into<Vec<RespFrame>>) -> Self {
         RespSet(s.into())
+    }
+}
+
+pub fn extract_simaple_frame_data(buf: &[u8], prefix: &str) -> Result<usize, RespError> {
+    if buf.len() < 3 {
+        return Err(RespError::NotComplete);
+    }
+    if !buf.starts_with(prefix.as_bytes()) {
+        return Err(RespError::InvalidFrameType(format!(
+            "expect: {}, got: {:?}",
+            prefix, buf
+        )));
+    }
+    let mut end = 0;
+    for i in 0..buf.len() - 1 {
+        if buf[i] == b'\r' && buf[i + 1] == b'\n' {
+            end = i;
+            break;
+        }
+    }
+    if end == 0 {
+        return Err(RespError::NotComplete);
+    }
+    Ok(end)
+}
+
+pub fn extract_fixed_data(
+    buf: &mut BytesMut,
+    expect: &str,
+    expect_type: &str,
+) -> Result<(), RespError> {
+    if buf.len() < 3 {
+        return Err(RespError::NotComplete);
+    }
+    if !buf.starts_with(expect.as_bytes()) {
+        return Err(RespError::InvalidFrameType(format!(
+            "expect: {}, got: {:?}",
+            expect_type, buf
+        )));
+    }
+    buf.advance(expect.len());
+    Ok(())
+}
+
+pub fn find_ctrl(buf: &[u8], nth: usize) -> Option<usize> {
+    let mut count = 0;
+    for i in 0..buf.len() - 1 {
+        if buf[i] == b'\r' && buf[i + 1] == b'\n' {
+            count += 1;
+            if count == nth {
+                return Some(i);
+            }
+        }
+    }
+    None
+}
+
+// 计算结束位置以及获取长度信息
+pub fn parse_length(buf: &[u8], prefix: &str) -> Result<(usize, usize), RespError> {
+    let end = extract_simaple_frame_data(buf, prefix)?;
+    let s = String::from_utf8_lossy(&buf[prefix.len()..end]);
+    Ok((end, s.parse()?))
+}
+
+// 计算所有的字节长度
+pub fn calc_total_length(
+    buf: &[u8],
+    end: usize,
+    len: usize,
+    prefix: &str,
+) -> Result<usize, RespError> {
+    let mut total = end + CRLF_LEN;
+    let mut data = &buf[total..];
+    match prefix {
+        "*" | "~" => {
+            for _ in 0..len {
+                let len = RespFrame::expect_length(data)?;
+                data = &data[len..];
+                total += len;
+            }
+            Ok(total)
+        }
+        "%" => {
+            for _ in 0..len {
+                let len = RespFrame::expect_length(data)?;
+                data = &data[len..];
+                total += len;
+
+                let len = RespFrame::expect_length(data)?;
+                data = &data[len..];
+                total += len;
+            }
+            Ok(total)
+        }
+        _ => Ok(len + CRLF_LEN),
     }
 }
