bae224d24e34e7ca04c4c9ceedc2da63d1d84d7a
zhouzhangqi <1978892854@qq.com>
2024-05-14 22:52:58 +0800

    refactor: move each frame type to its own file

--- src/resp/mod.rs
+++ src/resp/mod.rs
@@ -1,14 +1,33 @@
-mod decode;
-mod encode;
+mod array;
+mod bool;
+mod bulk_string;
+mod double;
+mod frame;
+mod integer;
+mod map;
+mod null;
+mod set;
+mod simple_error;
+mod simple_string;
 
 use bytes::{Buf, BytesMut};
 use enum_dispatch::enum_dispatch;
-use std::collections::HashMap;
-use std::ops::{Deref, DerefMut};
 use thiserror::Error;
+
+pub use self::{
+    array::{RespArray, RespNullArray},
+    bulk_string::{BulkString, RespNullBulkString},
+    frame::RespFrame,
+    map::RespMap,
+    null::RespNull,
+    set::RespSet,
+    simple_error::SimpleError,
+    simple_string::SimpleString,
+};
 
 const CRLF: &[u8] = b"\r\n";
 const CRLF_LEN: usize = CRLF.len();
+const BUF_CAP: usize = 4096;
 
 #[enum_dispatch]
 pub trait RespEncode {
@@ -38,142 +57,6 @@ pub enum RespError {
     Utf8Error(#[from] std::string::FromUtf8Error),
     #[error("Parse float error: {0}")]
     ParseFloatError(#[from] std::num::ParseFloatError),
-}
-
-#[enum_dispatch(RespEncode)]
-#[derive(Debug, Clone, PartialEq)]
-pub enum RespFrame {
-    SimpleString(SimpleString),
-    Error(SimpleError),
-    Integer(i64),
-    BulkString(BulkString),
-    NullBulkString(RespNullBulkString),
-    Array(RespArray),
-    Null(RespNull),
-    NullArray(RespNullArray),
-    Boolean(bool),
-    Double(f64),
-    Map(RespMap),
-    Set(RespSet),
-}
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct SimpleString(pub(crate) String);
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct SimpleError(pub(crate) String);
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct BulkString(pub(crate) Vec<u8>);
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct RespArray(pub(crate) Vec<RespFrame>);
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct RespNull;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct RespNullArray;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct RespNullBulkString;
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct RespMap(pub(crate) HashMap<String, RespFrame>);
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct RespSet(pub(crate) Vec<RespFrame>);
-
-impl Deref for SimpleString {
-    type Target = String;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl Deref for SimpleError {
-    type Target = String;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl Deref for BulkString {
-    type Target = Vec<u8>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl Deref for RespArray {
-    type Target = Vec<RespFrame>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl Deref for RespMap {
-    type Target = HashMap<String, RespFrame>;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for RespMap {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl Deref for RespSet {
-    type Target = Vec<RespFrame>;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl SimpleString {
-    pub fn new(s: impl Into<String>) -> Self {
-        SimpleString(s.into())
-    }
-}
-
-impl SimpleError {
-    pub fn new(s: impl Into<String>) -> Self {
-        SimpleError(s.into())
-    }
-}
-
-impl BulkString {
-    pub fn new(s: impl Into<Vec<u8>>) -> Self {
-        BulkString(s.into())
-    }
-}
-
-impl RespArray {
-    pub fn new(s: impl Into<Vec<RespFrame>>) -> Self {
-        RespArray(s.into())
-    }
-}
-
-impl Default for RespMap {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl RespMap {
-    pub fn new() -> Self {
-        RespMap(HashMap::new())
-    }
-}
-
-impl RespSet {
-    pub fn new(s: impl Into<Vec<RespFrame>>) -> Self {
-        RespSet(s.into())
-    }
 }
 
 pub fn extract_simaple_frame_data(buf: &[u8], prefix: &str) -> Result<usize, RespError> {
@@ -268,58 +151,5 @@ pub fn calc_total_length(
             Ok(total)
         }
         _ => Ok(len + CRLF_LEN),
-    }
-}
-impl From<&str> for SimpleString {
-    fn from(s: &str) -> Self {
-        SimpleString(s.to_string())
-    }
-}
-
-impl From<&str> for RespFrame {
-    fn from(s: &str) -> Self {
-        SimpleString(s.to_string()).into()
-    }
-}
-impl From<&str> for SimpleError {
-    fn from(s: &str) -> Self {
-        SimpleError(s.to_string())
-    }
-}
-impl From<&str> for BulkString {
-    fn from(s: &str) -> Self {
-        BulkString(s.as_bytes().to_vec())
-    }
-}
-impl From<&[u8]> for BulkString {
-    fn from(value: &[u8]) -> Self {
-        BulkString(value.to_vec())
-    }
-}
-impl From<&[u8]> for RespFrame {
-    fn from(value: &[u8]) -> Self {
-        BulkString(value.to_vec()).into()
-    }
-}
-
-impl<const N: usize> From<&[u8; N]> for BulkString {
-    fn from(value: &[u8; N]) -> Self {
-        BulkString(value.to_vec())
-    }
-}
-impl<const N: usize> From<&[u8; N]> for RespFrame {
-    fn from(value: &[u8; N]) -> Self {
-        BulkString(value.to_vec()).into()
-    }
-}
-impl AsRef<str> for SimpleString {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
-}
-
-impl AsRef<[u8]> for BulkString {
-    fn as_ref(&self) -> &[u8] {
-        &self.0
     }
 }
